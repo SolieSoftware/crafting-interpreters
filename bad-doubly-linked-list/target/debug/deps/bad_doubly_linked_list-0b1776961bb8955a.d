c417e2ab67e1643e432c70289d1a6d72304e2a0c
Sol Shortland <sol.shortland@gmail.com>
2026-03-28 11:36:34 +0000

    Adding in rust extensions. And implementing push method for bad doubly linked list

---
+++ bad-doubly-linked-list/target/debug/deps/bad_doubly_linked_list-0b1776961bb8955a.d
@@ -0,0 +1,6 @@
+/Users/solshortland/projects/crafting-interpreters/bad-doubly-linked-list/target/debug/deps/bad_doubly_linked_list-0b1776961bb8955a.d: src/lib.rs src/first.rs
+
+/Users/solshortland/projects/crafting-interpreters/bad-doubly-linked-list/target/debug/deps/libbad_doubly_linked_list-0b1776961bb8955a.rmeta: src/lib.rs src/first.rs
+
+src/lib.rs:
+src/first.rs:
---
+++ bad-doubly-linked-list/target/debug/deps/bad_doubly_linked_list-7785405bd44b67b1.d
@@ -0,0 +1,5 @@
+/Users/solshortland/projects/crafting-interpreters/bad-doubly-linked-list/target/debug/deps/bad_doubly_linked_list-7785405bd44b67b1.d: src/main.rs
+
+/Users/solshortland/projects/crafting-interpreters/bad-doubly-linked-list/target/debug/deps/libbad_doubly_linked_list-7785405bd44b67b1.rmeta: src/main.rs
+
+src/main.rs:
