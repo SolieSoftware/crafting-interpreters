a586bcb0c3c04c9e12a7a80ccbd967726471501d
Sol Shortland <sol.shortland@gmail.com>
2026-04-05 20:36:52 +0100

    Adding in iterator

--- linked-list/src/second.rs
+++ linked-list/src/second.rs
@@ -1,4 +1,6 @@
-
+// Tuple structs are an alternative form of struct
+// useful for trivial wrappers around other types.
+pub struct IntoIter<T>(List<T>);
 pub struct List<T> {
     head: Link<T>,
 }
@@ -42,6 +44,10 @@ impl<T> List<T> {
             &mut node.elem
         })
     }
+
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -51,6 +57,14 @@ impl<T> Drop for List<T> {
             cur_link = boxed_node.next.take();
         }
     }   
+}
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // access fields of a tuple struct numerically
+        self.0.pop()
+    }
 }
 
 
@@ -86,21 +100,33 @@ mod test {
         assert_eq!(list.pop(), None);
     }
 
-#[test]
-fn peek() {
-    let mut list = List::new();
-    assert_eq!(list.peek(), None);
-    assert_eq!(list.peek_mut(), None);
-    list.push(1); list.push(2); list.push(3);
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
 
-    assert_eq!(list.peek(), Some(&3));
-    assert_eq!(list.peek_mut(), Some(&mut 3));
-    list.peek_mut().map(|value| {
-        *value = 42
-    });
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+        list.peek_mut().map(|value| {
+            *value = 42
+        });
 
-    assert_eq!(list.peek(), Some(&42));
-    assert_eq!(list.pop(), Some(42));
-}
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter() {
+        let mut list = List::new();
+        list.push(1); list.push(2); list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
 
 }
