beb7f6dccf61ce51f6d380d395caa97c3a55ad2d
Sol Shortland <sol.shortland@gmail.com>
2026-04-08 07:35:45 +0100

    Completed implementation of iterable mutable list

--- linked-list/target/debug/deps/bad_doubly_linked_list-0b1776961bb8955a.d
+++ linked-list/target/debug/deps/bad_doubly_linked_list-0b1776961bb8955a.d
@@ -1,7 +1,8 @@
-/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/bad_doubly_linked_list-0b1776961bb8955a.d: src/lib.rs src/first.rs src/second.rs
+/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/bad_doubly_linked_list-0b1776961bb8955a.d: src/lib.rs src/first.rs src/second.rs src/third.rs
 
-/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/libbad_doubly_linked_list-0b1776961bb8955a.rmeta: src/lib.rs src/first.rs src/second.rs
+/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/libbad_doubly_linked_list-0b1776961bb8955a.rmeta: src/lib.rs src/first.rs src/second.rs src/third.rs
 
 src/lib.rs:
 src/first.rs:
 src/second.rs:
+src/third.rs:
