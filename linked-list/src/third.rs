143f9f7d6328b5f54624cb4f0fe1ce39f0e5591d
Sol Shortland <sol.shortland@gmail.com>
2026-04-08 11:43:06 +0100

    Structure and constructors of a bad but safe doubly linked list

--- linked-list/src/third.rs
+++ linked-list/src/third.rs
@@ -58,7 +58,18 @@ impl<'a, T> Iterator for Iter<'a, T> {
     }
 }
 
-
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut head = self.head.take();
+        while let Some(node) = head {
+            if let Ok(mut node) = Rc::try_unwrap(node) {
+                head = node.next.take();
+            } else {
+                break;
+            }
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -86,7 +97,7 @@ mod test {
         assert_eq!(list.head(), None);
 
     }
-    
+
     #[test]
     fn iter() {
         let list = List::new().prepend(1).prepend(2).prepend(3);
