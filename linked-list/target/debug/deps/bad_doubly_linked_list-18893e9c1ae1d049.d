d3633060b773cc2584ecb9e56ff3920cf9bf74f8
Sol Shortland <sol.shortland@gmail.com>
2026-04-08 07:58:39 +0100

    Implementation a persistent singly linked stack, using reference counter

--- linked-list/target/debug/deps/bad_doubly_linked_list-18893e9c1ae1d049.d
+++ linked-list/target/debug/deps/bad_doubly_linked_list-18893e9c1ae1d049.d
@@ -1,7 +1,8 @@
-/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/bad_doubly_linked_list-18893e9c1ae1d049.d: src/lib.rs src/first.rs src/second.rs
+/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/bad_doubly_linked_list-18893e9c1ae1d049.d: src/lib.rs src/first.rs src/second.rs src/third.rs
 
-/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/bad_doubly_linked_list-18893e9c1ae1d049: src/lib.rs src/first.rs src/second.rs
+/Users/solshortland/projects/crafting-interpreters/linked-list/target/debug/deps/bad_doubly_linked_list-18893e9c1ae1d049: src/lib.rs src/first.rs src/second.rs src/third.rs
 
 src/lib.rs:
 src/first.rs:
 src/second.rs:
+src/third.rs:
